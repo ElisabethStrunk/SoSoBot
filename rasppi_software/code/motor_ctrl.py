9daa38008d54a0bd860e07d6b621235d15fddfd5
Corvin Röndigs <C.Roendigs@salt-and-pepper.eu>
2019-05-09 14:10:22 +0200

    Created pyhton scirpt/class for controlling the motors (not tested and just for normal gpios)

---
+++ rasppi_software/code/motor_ctrl.py
@@ -0,0 +1,44 @@
+
+#***************************************************************
+#	Imports
+#***************************************************************
+import smbus
+import os
+import RPi.GPIO as GPIO
+
+#***************************************************************
+#	Globals
+#***************************************************************
+
+#***************************************************************
+#	Global-Functions
+#***************************************************************
+class MotorCtrl:
+
+    """Control of a servo-motor.
+
+    Keyword arguments:
+    gpioPin -- The gpio-pin which is connected to the motor-driver
+    """
+
+    # Initializes the motor-control
+	def __init__(self, gpio_pin, name):
+        self.pin = gpio_pin
+        self.name = name
+        self.on  = True
+
+    # Stops the motor
+    def start(self):
+        print('Motor:= ; started', self.name)
+        GPIO.output(self.pin, 1)
+        self.on  = True
+
+    # Starts the motor
+    def stop(self)
+        print('Motor:= ; stopped', self.name)
+        GPIO.output(self.pin, 0)
+        self.on  = False
+    
+    # Indicates if the motor is running
+    def is_on(self)
+        return self.on
--- rasppi_software/code/server.py
+++ rasppi_software/code/server.py
@@ -1,7 +1,25 @@
 
 from flask import Flask, request
 from flask_restful import Api, Resource
+import motor_ctrl
 
+
+#***************************************************************
+#	Globals
+#***************************************************************
+
+# GPIO-Pins
+MOTOR_LEFT			= 40
+MOTOR_RIGHT		    = 37
+
+
+motor_left      = motor_ctrl.MotorCtrl(MOTOR_RIGHT, 'Left')
+motor_right     = motor_ctrl.MotorCtrl(MOTOR_RIGHT, 'Right')
+
+
+#***************************************************************
+#	Global-Functions
+#***************************************************************
 
 class Motor(Resource):
     def put(self, direction):
@@ -11,28 +29,30 @@ class Motor(Resource):
             motor_status = None
         if direction == 'right':
             if motor_status == '1':
-                # TODO: call function for turning the right motor on
+                motor_right.start()
                 return {"message": "RIGHT motor turned ON"}, 200
             elif motor_status == '0':
-                # TODO: call function for turning the right motor off
+                motor_right.stop()
                 return {"message": "RIGHT motor turned OFF"}, 200
             else:
                 return {"message": "Parameter ON/OFF is either missing or not valid"}, 400
         elif direction == 'left':
             if motor_status == '1':
-                # TODO: call function for turning the left motor on
+                motor_left.start()
                 return {"message": "LEFT motor turned ON"}, 200
             elif motor_status == '0':
-                # TODO: call function for turning the left motor off
+                motor_left.stop()
                 return {"message": "LEFT motor turned OFF"}, 200
             else:
                 return {"message": "Parameter ON/OFF is either missing or not valid"}, 400
         elif direction == 'both':
             if motor_status == '1':
-                # TODO: call function for turning both motors on
+                motor_right.start()
+                motor_left.start()
                 return {"message": "BOTH motors turned ON"}, 200
             elif motor_status == '0':
-                # TODO: call function for turning both motors off
+                motor_right.stop()
+                motor_left.stop()
                 return {"message": "BOTH motors turned OFF"}, 200
             else:
                 return {"message": "Parameter ON/OFF is either missing or not valid"}, 400
