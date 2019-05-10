449eb54a689edfb7ebac65111c3ad9d82160821b
Corvin Röndigs <C.Roendigs@salt-and-pepper.eu>
2019-05-10 12:50:33 +0200

    Motor-ctrl: foward and backward are now avaible. Also added a setup for the gpios

--- rasppi_software/code/motor_ctrl.py
+++ rasppi_software/code/motor_ctrl.py
@@ -11,12 +11,22 @@ import RPi.GPIO as GPIO
 #***************************************************************
 
 # GPIO-Pins
-MOTOR_LEFT			= 40
-MOTOR_RIGHT		    = 37
+MOTOR_RIGHT_FORWARD = 37
+MOTOR_RIGHT_BACKWARD = 37
+MOTOR_LEFT_FORWARD = 40
+MOTOR_LEFT_BACKWARD	= 40
 
 
-motor_left      = motor_ctrl.MotorCtrl(MOTOR_RIGHT, 'Left')
-motor_right     = motor_ctrl.MotorCtrl(MOTOR_RIGHT, 'Right')
+#***************************************************************
+#	Setup
+#***************************************************************
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(MOTOR_RIGHT_FORWARD,GPIO.OUT)
+GPIO.setup(MOTOR_RIGHT_FORWARD,GPIO.OUT)
+GPIO.setup(MOTOR_LEFT_FORWARD,GPIO.OUT)
+GPIO.setup(MOTOR_LEFT_BACKWARD,GPIO.OUT)
+
 
 
 #***************************************************************
@@ -31,23 +41,41 @@ class MotorCtrl:
     """
 
     # Initializes the motor-control
-	def __init__(self, gpio_pin, name):
-        self.pin = gpio_pin
+	def __init__(self, gpio_pin_forward, gpio_pin_backward, name):
+        self.pin_forward = gpio_pin_forward
+        self.pin_backward = gpio_pin_backward
         self.name = name
         self.on  = True
 
-    # Stops the motor
-    def start(self):
-        print('Motor:= ; started', self.name)
-        GPIO.output(self.pin, 1)
+    # Starts the motor in forward direction
+    def forward(self):
+        print('Motor:= ; started forward', self.name)
+        GPIO.output(self.pin_backward, 0)
+        GPIO.output(self.pin_forward, 1)
+        self.on  = True
+
+        
+    # Starts the motor in backward direction
+    def forward(self):
+        print('Motor:= ; started backward', self.name)
+        GPIO.output(self.pin_forward, 0)
+        GPIO.output(self.pin_backward, 1)
         self.on  = True
 
     # Starts the motor
     def stop(self)
         print('Motor:= ; stopped', self.name)
-        GPIO.output(self.pin, 0)
+        GPIO.output(self.pin_forward, 0)
+        GPIO.output(self.pin_forward, 0)
         self.on  = False
     
     # Indicates if the motor is running
     def is_on(self)
         return self.on
+
+
+#***************************************************************
+#	Objects
+#***************************************************************
+motor_right = motor_ctrl.MotorCtrl(MOTOR_RIGHT_FORWARD, MOTOR_LEFT_BACKWARD, 'Right')
+motor_left = motor_ctrl.MotorCtrl(MOTOR_LEFT_FORWARD, MOTOR_LEFT_BACKWARD, 'Left')
