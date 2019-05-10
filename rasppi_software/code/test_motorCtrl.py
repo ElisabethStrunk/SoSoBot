746cfa1800cfb719d456227063b3f788c98327d4
Corvin Röndigs <C.Roendigs@salt-and-pepper.eu>
2019-05-10 16:12:24 +0200

    Added also tests for driving the wheel backward

--- rasppi_software/code/test_motorCtrl.py
+++ rasppi_software/code/test_motorCtrl.py
@@ -1,25 +1,33 @@
 from motor_ctrl import motor_left, motor_right
 import time
+import sys
+import RPi.GPIO as GPIO
 
 #***************************************************************
 #	Global-Functions
 #***************************************************************
 
 try:
-    while True:
-        print('Start Test:')
-        time.sleep(5)
-        print('Test: Turn on right motor')
-        motor_right.forward()
-        time.sleep(5)
-        print('Test: Turn off right motor')
-        motor_right.stop()
-        time.sleep(5)
-        print('Test: Turn on left motor')
-        motor_left.forward()
-        time.sleep(5)
-        print('Test: Turn off left motor')
-        motor_left.stop()
+  while True:
+    print('Start Test:')
+    time.sleep(5)
+    print('Test: Turn on right motor forward')
+    motor_right.forward()
+    time.sleep(5)
+    print('Test: Turn on right motor backward')
+    motor_right.backward()
+    time.sleep(5)
+    print('Test: Turn off right motor')
+    motor_right.stop()
+    time.sleep(5)
+    print('Test: Turn on left motor forward')
+    motor_left.forward()
+    time.sleep(5)
+    print('Test: Turn on right motor backward')
+    motor_left.backward()
+    time.sleep(5)
+    print('Test: Turn off left motor')
+    motor_left.stop()
           
 except KeyboardInterrupt:
 	GPIO.cleanup()
