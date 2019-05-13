38159b0a15b18c0c854e5c67f3cc4d6dc0c2d873
ThorbenKay <thorben.kay@haw-hamburg.de>
2019-05-13 16:55:10 +0200

    Refactored REST-API according to new Interface Definitions. Changed PUT to GET. (Tested with http request via browser)

--- rasppi_software/code/server.py
+++ rasppi_software/code/server.py
@@ -8,47 +8,54 @@ from motor_ctrl import motor_left, motor_right
 #***************************************************************
 
 class Motor(Resource):
-    def put(self, direction):
-        try:
-            motor_status = request.args.get('on')
-        except:
-            motor_status = None
+    def get(self, direction, status):
         if direction == 'right':
-            if motor_status == '1':
+            if status == 'on':
                 motor_right.forward()
-                return {"message": "RIGHT motor turned ON"}, 200
-            elif motor_status == '0':
+                return {"message": "RIGHT movement started"}, 200
+            elif status == 'off':
                 motor_right.stop()
-                return {"message": "RIGHT motor turned OFF"}, 200
+                return {"message": "RIGHT movement stopped"}, 200
             else:
                 return {"message": "Parameter ON/OFF is either missing or not valid"}, 400
         elif direction == 'left':
-            if motor_status == '1':
+            if status == 'on':
                 motor_left.forward()
-                return {"message": "LEFT motor turned ON"}, 200
-            elif motor_status == '0':
+                return {"message": "LEFT movement started"}, 200
+            elif status == 'off':
                 motor_left.stop()
-                return {"message": "LEFT motor turned OFF"}, 200
+                return {"message": "LEFT movement stopped"}, 200
             else:
                 return {"message": "Parameter ON/OFF is either missing or not valid"}, 400
-        elif direction == 'both':
-            if motor_status == '1':
+        elif direction == 'forward':
+            if status == 'on':
                 motor_right.forward()
                 motor_left.forward()
-                return {"message": "BOTH motors turned ON"}, 200
-            elif motor_status == '0':
+                return {"message": "FORWARD movement started"}, 200
+            elif status == 'off':
                 motor_right.stop()
                 motor_left.stop()
-                return {"message": "BOTH motors turned OFF"}, 200
+                return {"message": "BACKWARD movement started"}, 200
+            else:
+                return {"message": "Parameter ON/OFF is either missing or not valid"}, 400
+        elif direction == 'backward':
+            if status == 'on':
+                motor_right.backwards()
+                motor_left.backwards()
+                return {"message": "BACKWARD movement started"}, 200
+            elif status == 'off':
+                motor_right.stop()
+                motor_left.stop()
+                return {"message": "BACKWARD movement stopped"}, 200
             else:
                 return {"message": "Parameter ON/OFF is either missing or not valid"}, 400
         else:
-            return {"message": "Motor '{}' not found".format(direction)}, 400
+            return {"message": "Direction '{}' not found".format(direction)}, 400
 
 
 app = Flask(__name__)
 api = Api(app)
-api.add_resource(Motor, '/motor/<string:direction>')
+api.add_resource(Motor, '/<string:direction>/<string:status>')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
