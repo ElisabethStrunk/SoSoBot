5f32b8c682c3bfe9e80a186dd3afb75d8efb68a9
Aaron Krämer <9316187+aroyarexs@users.noreply.github.com>
2019-05-24 16:25:02 +0200

    Designs UI

--- app/SoSoBotAndroid/app/src/main/java/saltandpepper/software/sosobotandroid/MainActivity.kt
+++ app/SoSoBotAndroid/app/src/main/java/saltandpepper/software/sosobotandroid/MainActivity.kt
@@ -2,11 +2,30 @@ package saltandpepper.software.sosobotandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    private val robotConnection = RobotConnection("192.168.101.62")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onForwardButtonPressed(view: View) {
+        robotConnection.forward(true)
+    }
+
+    fun onBackwardButtonPressed(view: View) {
+        robotConnection.backward(true)
+    }
+
+    fun onLeftButtonPressed(view: View) {
+        robotConnection.left(true)
+    }
+
+    fun onRightButtonPressed(view: View) {
+        robotConnection.right(true)
     }
 }
