5a21e5b3954acba26c994be65c00cee3260f9872
Corvin Röndigs <C.Roendigs@salt-and-pepper.eu>
2019-05-26 01:38:42 +0200

    Improved MotorCtrl class

--- rasppi_software/code/motor_ctrl.py
+++ rasppi_software/code/motor_ctrl.py
@@ -9,39 +9,26 @@ import threading
 from enum import Enum
 
 #***************************************************************
-#	Globals
+#	Globals / Constants / Enums
 #***************************************************************
 
-# GPIO-Pins (12,13,18 and 19 are capable of PWM)
+# Constants for GPIO-Pins
 MOTOR_RIGHT_FORWARD = 23
 MOTOR_RIGHT_BACKWARD = 24
 MOTOR_LEFT_FORWARD = 17
 MOTOR_LEFT_BACKWARD = 27
+
+# Constant for PWM Frequency
 PWM_FREQUENCY = 10000
 
-#***************************************************************
-#	Setup
-#***************************************************************
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(MOTOR_RIGHT_FORWARD,GPIO.OUT)
-GPIO.setup(MOTOR_RIGHT_BACKWARD,GPIO.OUT)
-GPIO.setup(MOTOR_LEFT_FORWARD,GPIO.OUT)
-GPIO.setup(MOTOR_LEFT_BACKWARD,GPIO.OUT)
-
+# Enum of direction
 class direction(Enum):
   FORWARD  = 1
   BACKWARD = 2
   STOP = 3
 
 #***************************************************************
-#	Private-Functions
-#***************************************************************
-# Starts the motor in forward direction
-
-
-#***************************************************************
-#	Global-Functions
+#	Class definition
 #***************************************************************
 class MotorCtrl:
 
@@ -51,85 +38,93 @@ class MotorCtrl:
   gpioPin -- The gpio-pin which is connected to the motor-driver
   """
 
-  # Initializes the motor-control
+  # Initializes a handle for a control-motor
   def __init__(self, gpio_pin_forward, gpio_pin_backward, name):
     self.pin_forward = gpio_pin_forward
     self.pin_backward = gpio_pin_backward
-    self.name = name
+    self.name = 'Motor_' + name
     self.on  = False
-    
     self.direct = direction.STOP
     self.t_on = 0
     self.t_off =  0
+    self.timer = None
 
-  #
-  def run(self, direct, velocity):
-    self.direct = direct
-    self.t_off =  0
+  def forward(self, velocity = 1.0):
+    self._stop_timer()
+    self.direct = direction.FORWARD
+    _run(velocity)
 
-    # calculates the duty_cycle by velocity
-    if velocity is None:
-      duty_cycle = 1
-    else:
-      duty_cycle = velocity / 100
+  def backward(self, velocity = 1.0):
+    self._stop_timer()
+    self.direct = direction.BACKWARD
+    _run(velocity)
 
-    # calculates t_on and t_off for the PWM
-    period = 1 / PWM_FREQUENCY
-    self.t_on = duty_cycle * period
-    self.t_off = period - self.t_on  
-    print(self.name + ': ' + 't_on = ' + str(self.t_on) + 't_off = ' + str(self.t_off))
-    self.__run()
-
-   # Stops the motor
   def stop(self):
-    GPIO.output(self.pin_forward, 0)
-    GPIO.output(self.pin_backward, 0)
-    self.on  = False
-  
-  # Starts the motor in forward direction
-  def __forward(self):
-    GPIO.output(self.pin_backward, 0)
-    GPIO.output(self.pin_forward, 1)
-    self.on  = True
-        
-  # Starts the motor in backward direction
-  def __backward(self):
-    GPIO.output(self.pin_forward, 0)
-    GPIO.output(self.pin_backward, 1)
-    self.on  = True
+    self._stop_timer()
+    self.direct = direction.STOP
+    self._stop()
 
-  def __run(self):
-
-    if self.direct == direction.FORWARD:
-      if self.on == True:
-        self.stop()
-      else:
-        self.__forward()
-    elif self.direct == direction.BACKWARD:
-      if self.on == True:
-        self.stop()
-      else:
-        self.__backward()
-     
-    if self.t_off <= 0.0:
-      if self.direct == direction.FORWARD:
-        self.__forward()
-      else:
-        self.__backward()
-    elif self.t_on <= 0.0:
-       self.stop()
-    elif self.on == True:
-      threading.Timer(self.t_on, self.__run).start()
-    elif self.on == False:
-      threading.Timer(self.t_off, self.__run).start()
-    
   # Indicates if the motor is running
   def is_on(self):
     return self.on
 
+  def _run(velocity):
+    if velocity >= 1.0:
+      if self.direct == direction.FORWARD:
+        self._forward()
+      elif self.direct == direction.BACKWARD:
+        self._backward()
+    elif velocity <= 0.0:
+      self._stop()
+    else:
+      duty_cycle = velocity
+      period = 1 / PWM_FREQUENCY
+      self.t_on = duty_cycle * period
+      self.t_off = period - self.t_on
+      _run_pwm()
+    
+  def _run_pwm(self):
+    if self.on:
+       self._stop()
+       self.timer = threading.Timer(self.t_off, self._run_pwm)
+       self.timer.start()
+    else:
+      if self.direct == direction.FORWARD:
+        self._forward()
+      elif self.direct == direction.BACKWARD:
+        self._backward()
+      self.timer = threading.Timer(self.t_off, self._run_pwm)
+      self.timer.start()
+        
+  def _forward(self):
+    GPIO.output(self.pin_backward, 0)
+    GPIO.output(self.pin_forward, 1)
+    self.on  = True
 
+  def _backward(self):
+    GPIO.output(self.pin_forward, 0)
+    GPIO.output(self.pin_backward, 1)
+    self.on  = True
+
+  def _stop(self):
+    GPIO.output(self.pin_forward, 0)
+    GPIO.output(self.pin_backward, 0)
+    self.on  = False 
+
+  def _stop_timer(self):
+    if self.timer != None:
+      self.timer.cancel()
+      self.timer = None
+    
 #***************************************************************
 #	Objects
 #***************************************************************
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(MOTOR_RIGHT_FORWARD,GPIO.OUT)
+GPIO.setup(MOTOR_RIGHT_BACKWARD,GPIO.OUT)
+GPIO.setup(MOTOR_LEFT_FORWARD,GPIO.OUT)
+GPIO.setup(MOTOR_LEFT_BACKWARD,GPIO.OUT)
+
 motor_right = MotorCtrl(MOTOR_RIGHT_FORWARD, MOTOR_RIGHT_BACKWARD, 'Right')
 motor_left  = MotorCtrl(MOTOR_LEFT_FORWARD, MOTOR_LEFT_BACKWARD, 'Left')
