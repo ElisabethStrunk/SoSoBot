c3bf90d3d6bfff63a1e8ee6884dd6bf2946cab28
Aaron <9316187+aroyarexs@users.noreply.github.com>
2019-05-27 21:04:01 +0200

    Connection to robot sends now power parameter

--- app/SoSoBotAndroid/app/src/main/java/saltandpepper/software/sosobotandroid/RobotConnection.kt
+++ app/SoSoBotAndroid/app/src/main/java/saltandpepper/software/sosobotandroid/RobotConnection.kt
@@ -5,28 +5,21 @@ import com.github.kittinunf.fuel.httpGet
 
 class RobotConnection(ipAddress: String) {
 
-    private val onPath = "/on/100.0"
-    private val offPath = "/off/0.0"
-
     init {
         FuelManager.instance.basePath = "http://$ipAddress"
     }
 
     fun move(direction: Direction, power: Byte = 100) {
-        // TODO update request for power parameter
-        request("/$direction", true)
+        request("/$direction/on/$power")
     }
 
     fun stop(direction: Direction) {
-        request("/$direction", false)
+        request("/$direction/off/0.0")
     }
 
-    private fun pathForBool(isOn: Boolean): String = if (isOn) onPath else offPath
-
-    private fun request(path: String, isOn: Boolean) {
-        val requestPath = path + pathForBool(isOn)
+    private fun request(path: String) {
         // TODO handle exception
-        requestPath.httpGet().responseString {
+        path.httpGet().responseString {
                 result ->
         }
     }
