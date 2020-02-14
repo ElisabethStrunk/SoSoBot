41fb882ceaef47312b5f4e38b4566d436ca3bfd8
Corvin Röndigs <C.Roendigs@salt-and-pepper.eu>
2020-02-14 14:31:26 +0100

    impliment test for motors

---
+++ rasppi_software/code/test_leni.py
@@ -0,0 +1,25 @@
+import sys
+import time
+import RPi.GPIO as GPIO
+from motor_ctrl import motor_left, motor_right
+
+try:
+ while True:
+  print('left forward')
+  motor_left.forward(1.0)
+  time.sleep(5.0)
+  print('left backward')
+  motor_left.backward(1.0)
+  time.sleep(5.0)
+  print('right forward')
+  motor_right.forward(1.0)
+  time.sleep(5.0)
+  print('right backward')
+  motor_right.backward(1.0)
+  break 
+  
+
+except KeyboardInterrupt:
+  print('program close')
+  GPIO.cleanup()
+  sys.exit()
